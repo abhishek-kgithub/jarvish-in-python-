3ed6061878cfa81d41114e4499296f4561a449ec
Abhishek Kumar <91193915+abhishek-kgithub@users.noreply.github.com>
2021-10-01 20:37:11 +0530

    Add files via upload

---
+++ jarvish.py
@@ -0,0 +1,50 @@
+import datetime
+
+import pyttsx3
+import speech_recognition as sr
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+def wishMe():
+    hour= int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("good morning!")
+
+    elif hour>=12 and hour<18:
+            speak("good afternoon!")      
+
+    else:
+        speak("good evening!")   
+
+    speak("i am jarvish sir. please tell me how can i help you")         
+
+def takeCommand():
+    r= sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1 
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said : {query}\n")
+
+    except Exception as e :
+        # print(e)    
+        print("say that again please...")
+        return "None"
+    return query
+
+if __name__== "__main__":
+    # speak("abhishek is good boy")
+    wishMe()
+    query = takeCommand().lower()
